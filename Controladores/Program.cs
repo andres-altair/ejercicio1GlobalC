83e789c64477c327f96521ab1b1f575b41ef8bf1
Andres <andresxiaomd12@gmail.com>
2024-04-01 14:40:12 +0200

    no message

--- Controladores/Program.cs
+++ Controladores/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace ejercicio1GlobalC.Controladores
+﻿using ejercicio1GlobalC.Dtos;
+
+namespace ejercicio1GlobalC.Controladores
 {
     class Program
     {
         static void Main(string[] args)
         {
+            List<BibliotecaDto>listaBiblioteca = new List<BibliotecaDto>();
+            List<ClienteDto> listaClientes = new List<ClienteDto>();
+            List<LibroDto> listaLibros = new List<LibroDto>();  
+            List<PrestamoDto>listaPrestamos = new List<PrestamoDto>();
+            if(listaBiblioteca.Count > 0)
+            {
+                bool abrir = false;
+                int opcion;
+                while(!abrir)
+                {
+                    switch (opcion)
+                    {
+                        case 0: abrir = true; 
+                            break;
+                        case 1: 
 
+                    }
+                }
+            }
         }
     }
 }
--- Dtos/LibroDto.cs
+++ Dtos/LibroDto.cs
@@ -8,5 +8,35 @@ namespace ejercicio1GlobalC.Dtos
 {
     internal class LibroDto
     {
+        long id;
+        string titulo = "aaaaaa";
+        string subtitulo = "aaaaa";
+        string autor = "aaaaa";
+        string ISBN = "aaaaa";
+        int numeroEdicion = 9999999;
+        string editorial = "aaaaaa";
+        int stock = 99999;
+
+        public LibroDto() { }   
+        public LibroDto(long id, string titulo, string subtitulo, string autor, string iSBN, int numeroEdicion, string editorial, int stock)
+        {
+            this.id = id;
+            this.titulo = titulo;
+            this.subtitulo = subtitulo;
+            this.autor = autor;
+            ISBN = iSBN;
+            this.numeroEdicion = numeroEdicion;
+            this.editorial = editorial;
+            this.stock = stock;
+        }
+
+        public long Id { get => id; set => id = value; }
+        public string Titulo { get => titulo; set => titulo = value; }
+        public string Subtitulo { get => subtitulo; set => subtitulo = value; }
+        public string Autor { get => autor; set => autor = value; }
+        public string ISBN1 { get => ISBN; set => ISBN = value; }
+        public int NumeroEdicion { get => numeroEdicion; set => numeroEdicion = value; }
+        public string Editorial { get => editorial; set => editorial = value; }
+        public int Stock { get => stock; set => stock = value; }
     }
 }
